98d4000340623b202b0675c1b543838275fce1c8
3x-haust <lyusungyun@icloud.com>
2026-06-23 03:05:43 +0900

    fix: ingress 프록시 신뢰 설정

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { SecurityConfig } from './config/security.config';
 import {
   getAvatarDirectory,
@@ -15,7 +16,9 @@ import * as express from 'express';
 import { mkdirSync } from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.set('trust proxy', 1);
+
   const uploadsDirectory = getUploadsDirectory();
   const avatarsDirectory = getAvatarDirectory();
   const noticeImagesDirectory = getNoticeImageDirectory();
